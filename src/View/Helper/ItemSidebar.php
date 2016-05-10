ce5ff7496727fdb0235342e99f1187a5a487e2ef
patrickmj <patrickmjchnm@gmail.com>
2016-05-10 18:56:12 -0400

    move item mapping sidebar to partial
    part of #40

--- src/View/Helper/ItemSidebar.php
+++ src/View/Helper/ItemSidebar.php
@@ -15,38 +15,12 @@ class ItemSidebar extends AbstractHelper
     public function __invoke()
     {
         $userRole = $this->user->getRole();
-        $html = "
-        <div id='item-data' class='sidebar flags'>
-            <a href='#' class='sidebar-close o-icon-close'><span class='screen-reader-text'>Close Me</span></a>
-            <h3>Item Data: </h3>
-
-            <p>Data from this column applies to the entire item being imported.</p>
-            <p>Settings here will override any corresponding settings in Basic Import Settings.</p>
-            <ul>
-                <li data-flag='column-itemset-id' data-flag-type='item-data'>
-                    <a href='#' class='button'><span>Item Set ID</span></a>
-                </li>
-                <li data-flag='column-resourcetemplate' data-flag-type='item-data'>
-                    <a href='#' class='button'><span>Resource Template Name</span></a>
-                </li>
-                <li data-flag='column-resourceclass' data-flag-type='item-data'>
-                    <a href='#' class='button'><span>Resource Class Term</span></a>
-                </li>
-                ";
-        if( ($userRole == 'global_admin') || ($userRole == 'site_admin')) {
-
-            $html .="<li data-flag='column-owneremail'>
-                    <a href='#' class='button'><span>Owner Email Address</span></a>
-                </li>";
-        }
-        $html .= "</ul>";
+        return $this->getView()->partial(
+            'common/item-sidebar',
+            [
+                'userRole' => $userRole,
+            ]
+        );
         
-        $html .= "<div class='item-mapping options'>";
-        $html .= "<h4>Options</h4>";
-        $html .= "<a href='#' class='button column-multivalue'><span>Use multivalue separator</span></a>";
-        $html .= "<p>(Only applies to Item Set ID)</p>";
-        $html .= "</div>";
-        $html .= "</div>";
-        return $html;
     }
 }
