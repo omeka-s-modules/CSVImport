33320f6a226585b899160aa8422d79a3d7bd564c
patrickmj <patrickmjchnm@gmail.com>
2016-05-10 19:04:58 -0400

    put media sidebar into partial
    part of #40

--- src/View/Helper/MediaSidebar.php
+++ src/View/Helper/MediaSidebar.php
@@ -24,27 +24,11 @@ class MediaSidebar extends AbstractHelper
             }
         }
 
-        $html = "
-            <div id='media-import' class='sidebar flags'>
-                <a href='#' class='sidebar-close o-icon-close'><span class='screen-reader-text'>Close Me</span></a>
-                <h3>Media Importing: </h3>
-                <ul>";
-
-        foreach($mediaForms as $type=>$mediaForm) {
-            $html .= "<li data-flag='$type' data-flag-type='media'>
-                <a href='#' class='button'><span>{$mediaForm['label']}</span></a>
-            </li>";
-        }
-
-        $html .= "</ul>";
-        
-        $html .= "<div class='media-mapping options'>";
-        $html .= "<h4>Options</h4>";
-        $html .= "<a href='#' class='button column-multivalue'><span>Use multivalue separator</span></a>";
-        $html .= "</div>";
-        
-        $html .= "</div>";
-        return $html;
-
+        return $this->getView()->partial(
+            'common/media-sidebar',
+            [
+                'mediaForms' => $mediaForms,
+            ]
+        );
     }
 }
