33b2ef1d091292835655a391dbde9ed2a6f19485
patrickmj <patrickmjchnm@gmail.com>
2016-05-16 13:11:27 -0400

    redo mappingform
    close #42

--- src/Form/MappingForm.php
+++ src/Form/MappingForm.php
@@ -1,13 +1,13 @@
 <?php
 namespace CSVImport\Form;
 
-use Omeka\Form\ItemForm;
+use Omeka\Form\Form;
 use Omeka\Form\Element\ResourceSelect;
 use Zend\Validator\Callback;
 use Zend\Form\Element\Select;
 
 
-class MappingForm extends ItemForm
+class MappingForm extends Form
 {
     public function buildForm()
     {
@@ -26,15 +26,79 @@ class MappingForm extends ItemForm
                 'id' => 'comment',
                 'class' => 'input-body',
             ),
-            //in theory this _should_ be the correct way to make this 1st, 
-            //instead of putting it before the parent build
-            //but not so much, so the parent comes after
-            array('priority' => 100000) 
         ));
-        
+
         if ($resourceType == 'items' || $resourceType == 'item_sets') {
-            parent::buildForm();
-            
+            $serviceLocator = $this->getServiceLocator();
+            $url = $serviceLocator->get('ViewHelperManager')->get('url');
+            $templateSelect = new ResourceSelect($serviceLocator);
+            $templateSelect
+                ->setName('o:resource_template[o:id]')
+                ->setAttribute('id', 'resource-template-select')
+                ->setAttribute('data-api-base-url', $url('api/default',
+                    ['resource' => 'resource_templates']))
+                ->setLabel('Resource Template') // @translate
+                ->setEmptyOption('Select Template') // @translate
+                ->setOption('info', 'A pre-defined template for resource creation.') // @translate
+                ->setResourceValueOptions(
+                    'resource_templates',
+                    [],
+                    function ($template, $serviceLocator) {
+                        return $template->label();
+                    }
+                );
+                $this->add($templateSelect);
+                $classSelect = new ResourceSelect($serviceLocator);
+                $classSelect
+                    ->setName('o:resource_class[o:id]')
+                    ->setAttribute('id', 'resource-class-select')
+                    ->setLabel('Class') // @translate
+                    ->setEmptyOption('Select Class') // @translate
+                    ->setOption('info', 'A type for the resource. Different types have different default properties attached to them.') // @translate
+                    ->setResourceValueOptions(
+                        'resource_classes',
+                        [],
+                        function ($resourceClass, $serviceLocator) {
+                            return [
+                                $resourceClass->vocabulary()->label(),
+                                $resourceClass->label()
+                            ];
+                        }
+                    );
+                $this->add($classSelect);
+                $inputFilter = $this->getInputFilter();
+                $inputFilter->add([
+                    'name' => 'o:resource_template[o:id]',
+                    'required' => false,
+                ]);
+                $inputFilter->add([
+                    'name' => 'o:resource_class[o:id]',
+                    'required' => false,
+                ]);
+            }
+
+            $itemSetSelect = new ResourceSelect($serviceLocator);
+            $itemSetSelect->setName('o:item_set')
+                ->setAttribute('required', false)
+                ->setAttribute('multiple', true)
+                ->setAttribute('id', 'select-item-set')
+                ->setAttribute('data-placeholder', $translator->translate('Select Item Sets...'))
+                ->setLabel($translator->translate('Item Sets'))
+                ->setOption('info', $translator->translate('Select Items Sets for this resource.'))
+                ->setResourceValueOptions(
+                    'item_sets',
+                    [],
+                    function ($itemSet, $serviceLocator) {
+                        return $itemSet->displayTitle();
+                    }
+                );
+            if (!$itemSetSelect->getValueOptions()) {
+                $itemSetSelect->setAttribute('disabled', true);
+                $itemSetSelect->setAttribute('data-placeholder',
+                    $translator->translate('No item sets exist'));
+            }
+            $this->add($itemSetSelect);
+
             if( ($userRole == 'global_admin') || ($userRole == 'site_admin')) {
                 $ownerSelect = new ResourceSelect($this->getServiceLocator());
                 $ownerSelect->setName('o:owner')
@@ -48,10 +112,7 @@ class MappingForm extends ItemForm
                             return $user->name();
                         }
                     );
-                
                 $this->add($ownerSelect);
-                $itemSetSelect = $this->get('o:item_set');
-                $itemSetSelect->setAttribute('multiple', true);
             }
 
             $this->add(array(
@@ -66,7 +127,6 @@ class MappingForm extends ItemForm
                     'class' => 'input-body',
                     'value' => ','
                 )
-        ));
+            ));
         }
-    }
 }
