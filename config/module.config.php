30ecea9d1ab14bfa35fbe3ac8f474dea32202f28
Daniel Berthereau <Daniel.github@Berthereau.net>
2017-11-06 00:00:00 +0100

    Moved default settings in config.

--- config/module.config.php
+++ config/module.config.php
@@ -50,6 +50,100 @@ return [
             'csvimport_imports' => Api\Adapter\ImportAdapter::class,
         ],
     ],
+    'router' => [
+        'routes' => [
+            'admin' => [
+                'child_routes' => [
+                    'csvimport' => [
+                        'type' => 'Literal',
+                        'options' => [
+                            'route' => '/csvimport',
+                            'defaults' => [
+                                '__NAMESPACE__' => 'CSVImport\Controller',
+                                'controller' => 'Index',
+                                'action' => 'index',
+                            ],
+                        ],
+                        'may_terminate' => true,
+                        'child_routes' => [
+                            'past-imports' => [
+                                'type' => 'Literal',
+                                'options' => [
+                                    'route' => '/past-imports',
+                                    'defaults' => [
+                                        '__NAMESPACE__' => 'CSVImport\Controller',
+                                        'controller' => 'Index',
+                                        'action' => 'past-imports',
+                                    ],
+                                ],
+                            ],
+                            'map' => [
+                                'type' => 'Literal',
+                                'options' => [
+                                    'route' => '/map',
+                                    'defaults' => [
+                                        '__NAMESPACE__' => 'CSVImport\Controller',
+                                        'controller' => 'Index',
+                                        'action' => 'map',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'navigation' => [
+        'AdminModule' => [
+            [
+                'label' => 'CSV Import',
+                'route' => 'admin/csvimport',
+                'resource' => 'CSVImport\Controller\Index',
+                'pages' => [
+                    [
+                        'label' => 'Import', // @translate
+                        'route' => 'admin/csvimport',
+                        'resource' => 'CSVImport\Controller\Index',
+                    ],
+                    [
+                        'label' => 'Import', // @translate
+                        'route' => 'admin/csvimport/map',
+                        'resource' => 'CSVImport\Controller\Index',
+                        'visible' => false,
+                    ],
+                    [
+                        'label' => 'Past Imports', // @translate
+                        'route' => 'admin/csvimport/past-imports',
+                        'controller' => 'Index',
+                        'action' => 'past-imports',
+                        'resource' => 'CSVImport\Controller\Index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
+    'js_translate_strings' => [
+        'Remove mapping', // @translate
+        'Undo remove mapping', // @translate
+        'Select an item type at the left before choosing a resource class.', // @translate
+        'Select an element at the left before choosing a property.', // @translate
+        'Please enter a valid language tag.', // @translate
+        'Set multivalue separator for all columns', // @translate
+        'Unset multivalue separator for all columns', // @translate
+        'Advanced settings', // @translate
+        'Identifier for', // @translate
+    ],
     'csv_import' => [
         'sources' => [
             'application/vnd.oasis.opendocument.spreadsheet' => Source\OpenDocumentSpreadsheet::class,
@@ -177,99 +271,5 @@ return [
                 'groups' => 'group',
             ],
         ],
-    ],
-    'router' => [
-        'routes' => [
-            'admin' => [
-                'child_routes' => [
-                    'csvimport' => [
-                        'type' => 'Literal',
-                        'options' => [
-                            'route' => '/csvimport',
-                            'defaults' => [
-                                '__NAMESPACE__' => 'CSVImport\Controller',
-                                'controller' => 'Index',
-                                'action' => 'index',
-                            ],
-                        ],
-                        'may_terminate' => true,
-                        'child_routes' => [
-                            'past-imports' => [
-                                'type' => 'Literal',
-                                'options' => [
-                                    'route' => '/past-imports',
-                                    'defaults' => [
-                                        '__NAMESPACE__' => 'CSVImport\Controller',
-                                        'controller' => 'Index',
-                                        'action' => 'past-imports',
-                                    ],
-                                ],
-                            ],
-                            'map' => [
-                                'type' => 'Literal',
-                                'options' => [
-                                    'route' => '/map',
-                                    'defaults' => [
-                                        '__NAMESPACE__' => 'CSVImport\Controller',
-                                        'controller' => 'Index',
-                                        'action' => 'map',
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ],
-    ],
-    'navigation' => [
-        'AdminModule' => [
-            [
-                'label' => 'CSV Import',
-                'route' => 'admin/csvimport',
-                'resource' => 'CSVImport\Controller\Index',
-                'pages' => [
-                    [
-                        'label' => 'Import', // @translate
-                        'route' => 'admin/csvimport',
-                        'resource' => 'CSVImport\Controller\Index',
-                    ],
-                    [
-                        'label' => 'Import', // @translate
-                        'route' => 'admin/csvimport/map',
-                        'resource' => 'CSVImport\Controller\Index',
-                        'visible' => false,
-                    ],
-                    [
-                        'label' => 'Past Imports', // @translate
-                        'route' => 'admin/csvimport/past-imports',
-                        'controller' => 'Index',
-                        'action' => 'past-imports',
-                        'resource' => 'CSVImport\Controller\Index',
-                    ],
-                ],
-            ],
-        ],
-    ],
-    'translator' => [
-        'translation_file_patterns' => [
-            [
-                'type' => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern' => '%s.mo',
-                'text_domain' => null,
-            ],
-        ],
-    ],
-    'js_translate_strings' => [
-        'Remove mapping', // @translate
-        'Undo remove mapping', // @translate
-        'Select an item type at the left before choosing a resource class.', // @translate
-        'Select an element at the left before choosing a property.', // @translate
-        'Please enter a valid language tag.', // @translate
-        'Set multivalue separator for all columns', // @translate
-        'Unset multivalue separator for all columns', // @translate
-        'Advanced settings', // @translate
-        'Identifier for', // @translate
     ],
 ];
