083bbb396e68ed3f01eca0dd8c938babdb669976
John Flatness <john@zerocrates.org>
2018-09-05 17:17:51 -0400

    Clear out more references to old multivalue toggle

--- config/module.config.php
+++ config/module.config.php
@@ -185,7 +185,6 @@ return [
             'csv_import_delimiter' => ',',
             'csv_import_enclosure' => '"',
             'csv_import_multivalue_separator' => ',',
-            'csv_import_multivalue_by_default' => false,
             'csv_import_rows_by_batch' => 20,
             'csv_import_global_language' => '',
             'csv_import_identifier_property' => '',
