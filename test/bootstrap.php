6db1d420afa21c51d372009bbe6eda9cc816bd23
John Flatness <john@zerocrates.org>
2018-12-07 13:18:39 -0500

    Remove tests
    We need to resolve some issues in the core to make these more usable,
    plus the current setup on this end is very brittle to API changes.

--- test/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-use OmekaTestHelper\Bootstrap;
-
-Bootstrap::bootstrap(__DIR__);
-Bootstrap::loginAsAdmin();
-Bootstrap::enableModule('CSVImport');
