f4ed0fc7dbcec1fbfe583edf6173a90ff21d8b0a
John Flatness <john@zerocrates.org>
2020-06-23 14:21:54 -0400

    Enable batching for update actions

--- src/Job/Import.php
+++ src/Job/Import.php
@@ -170,11 +170,10 @@ class Import extends AbstractJob
             $this->rowsByBatch = (int) $args['rows_by_batch'];
         }
 
-        // The core allows batch processes only for creation and deletion.
-        if (!in_array($args['action'], [self::ACTION_CREATE, self::ACTION_DELETE, self::ACTION_SKIP])
-            // It allows to identify resources too, so to use a new resource
-            // from a previous row.
-            || ($args['action'] === self::ACTION_CREATE && $this->resourceType === 'resources')
+        // Force row-at-a-time processing for mixed-resource imports to allow
+        // rows to freely reference data from prior rows
+        if ($this->resourceType === 'resources'
+            && !in_array($args['action'], [self::ACTION_DELETE, self::ACTION_SKIP])
         ) {
             $this->rowsByBatch = 1;
         }
