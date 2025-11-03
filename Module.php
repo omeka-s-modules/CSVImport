3b472ee166a29f34837fc134f88dad55dbe93e4f
Abel B <abel.boilevin@biblibre.com>
2025-11-03 15:39:03 +0100

    Change module version and database schema in Module.php

--- Module.php
+++ Module.php
@@ -21,26 +21,9 @@ class Module extends AbstractModule
     {
         $connection = $serviceLocator->get('Omeka\Connection');
         $sql = <<<'SQL'
-CREATE TABLE csvimport_import (
-  id INT AUTO_INCREMENT NOT NULL,
-  job_id INT NOT NULL,
-  undo_job_id INT DEFAULT NULL,
-  comment VARCHAR(255) DEFAULT NULL,
-  resource_type VARCHAR(255) NOT NULL,
-  has_err TINYINT(1) NOT NULL,
-  stats LONGTEXT NOT NULL COMMENT '(DC2Type:json_array)',
-  UNIQUE INDEX UNIQ_17B50881BE04EA9 (job_id),
-  UNIQUE INDEX UNIQ_17B508814C276F75 (undo_job_id),
-  PRIMARY KEY(id)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB;
-CREATE TABLE csvimport_entity (
-  id INT AUTO_INCREMENT NOT NULL,
-  job_id INT NOT NULL,
-  entity_id INT NOT NULL,
-  resource_type VARCHAR(255) NOT NULL,
-  INDEX IDX_84D382F4BE04EA9 (job_id),
-  PRIMARY KEY(id)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB;
+CREATE TABLE csvimport_mapping (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, created DATETIME NOT NULL, mapping LONGTEXT NOT NULL COMMENT '(DC2Type:json)', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
+CREATE TABLE csvimport_import (id INT AUTO_INCREMENT NOT NULL, job_id INT NOT NULL, undo_job_id INT DEFAULT NULL, comment VARCHAR(255) DEFAULT NULL, resource_type VARCHAR(255) NOT NULL, has_err TINYINT(1) NOT NULL, stats LONGTEXT NOT NULL COMMENT '(DC2Type:json_array)', UNIQUE INDEX UNIQ_17B50881BE04EA9 (job_id), UNIQUE INDEX UNIQ_17B508814C276F75 (undo_job_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
+CREATE TABLE csvimport_entity (id INT AUTO_INCREMENT NOT NULL, job_id INT NOT NULL, entity_id INT NOT NULL, resource_type VARCHAR(255) NOT NULL, INDEX IDX_84D382F4BE04EA9 (job_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
 ALTER TABLE csvimport_import ADD CONSTRAINT FK_17B50881BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id);
 ALTER TABLE csvimport_import ADD CONSTRAINT FK_17B508814C276F75 FOREIGN KEY (undo_job_id) REFERENCES job (id);
 ALTER TABLE csvimport_entity ADD CONSTRAINT FK_84D382F4BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id);
@@ -60,6 +43,7 @@ ALTER TABLE csvimport_import DROP FOREIGN KEY FK_17B508814C276F75;
 ALTER TABLE csvimport_import DROP FOREIGN KEY FK_17B50881BE04EA9;
 DROP TABLE IF EXISTS csvimport_entity;
 DROP TABLE IF EXISTS csvimport_import;
+DROP TABLE IF EXISTS csvimport_mapping;
 SQL;
         $sqls = array_filter(array_map('trim', explode(';', $sql)));
         foreach ($sqls as $sql) {
@@ -76,6 +60,17 @@ SQL;
 ALTER TABLE csvimport_import ADD stats LONGTEXT NOT NULL COMMENT '(DC2Type:json_array)';
 UPDATE csvimport_import SET stats = CONCAT('{"processed":{"', resource_type, '":', added_count, '}}');
 ALTER TABLE csvimport_import DROP added_count;
+SQL;
+            $sqls = array_filter(array_map('trim', explode(';', $sql)));
+            foreach ($sqls as $sql) {
+                $connection->exec($sql);
+            }
+        }
+
+        if (version_compare($oldVersion, '2.7.0', '<')) {
+            $connection = $serviceLocator->get('Omeka\Connection');
+            $sql = <<<'SQL'
+CREATE TABLE csvimport_entity (id INT AUTO_INCREMENT NOT NULL, job_id INT NOT NULL, entity_id INT NOT NULL, resource_type VARCHAR(255) NOT NULL, INDEX IDX_84D382F4BE04EA9 (job_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
 SQL;
             $sqls = array_filter(array_map('trim', explode(';', $sql)));
             foreach ($sqls as $sql) {
