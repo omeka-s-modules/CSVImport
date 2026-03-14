f9e12bd551ee0da0b9f202f666adb2a7d3811433
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-03-14 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/Form/ImportFormFactory.php
+++ src/Service/Form/ImportFormFactory.php
@@ -2,7 +2,7 @@
 namespace CSVImport\Service\Form;
 
 use CSVImport\Form\ImportForm;
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class ImportFormFactory implements FactoryInterface
